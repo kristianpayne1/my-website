726b3c4b963783dab22ebaef815cbd8bb52c7f83
Kristian Payne <kjp29@kent.ac.uk>
2020-06-10 22:36:56 +0100

    CSS fixes

--- src/Home.js
+++ src/Home.js
@@ -27,7 +27,7 @@ class Home extends Component {
                     <div className="home" onScroll={this.handleScroll}>
                         <Fade>
                             <Image src={require('./img/me.jpg')} />
-                            <h1 className="home-title">{"I'm a "}<span style={{ color: "#98c379" }}>{"full stack"}</span> <span style={{ color: "#61afef" }}>{"web developer"}</span></h1>
+                            <h1 className="home-title">{"I'm a "}<span style={{ color: "#98c379" }}>{"full stack"}</span> <span style={{ color: "#61afef" }}>{"\nweb developer"}</span></h1>
                         </Fade>
                         <Fade bottom>
                             <p className="aboutme-para">Born in Finland, raised in the UK. I am a graduate from the University of Kent with a passion for development.</p>
