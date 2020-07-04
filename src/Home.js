04306dc3eede4791b43c20e4b8b018aeb0d17a5c
Kristian Payne <kjp29@kent.ac.uk>
2020-07-04 01:25:47 +0100

    Improved home

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image } from 'react-bootstrap/';
+import { Image, Container, Row, Col, } from 'react-bootstrap/';
 import { Fade } from 'react-reveal/';
 import Skills from './Skills.js';
 import Projects from './Projects.js';
@@ -8,14 +8,26 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <div className="home" onScroll={this.handleScroll} id="home">
-                    <Fade>
-                        <Image src={require('./img/me.jpg')} />
-                        <h1 className="home-title">{"I'm a "}<span style={{ color: "#98c379" }}>{"full stack"}</span> <span style={{ color: "#61afef" }}>{"\nweb developer"}</span></h1>
-                    </Fade>
-                    <Fade bottom>
-                        <p className="aboutme-para">Born in Finland, raised in the UK. I am a graduate from the University of Kent with a passion for development.</p>
-                    </Fade>
+                <div className="thing">
+                    <div className="home" id="home">
+                        <Container fluid>
+                            <Row className="align-items-center">
+                                <Col sm={{ order: 12 }}>
+                                    <Fade>
+                                        <Image src={require('./img/me.jpg')} />
+                                    </Fade>
+                                </Col>
+                                <Col sm={{ order: 1 }}>
+                                    <Fade>
+                                        <h1 className="home-title">{"I'm a "}<span style={{ color: "#98c379" }}>{"full stack"}</span> <span style={{ color: "#61afef" }}>{"\nweb developer"}</span></h1>
+                                    </Fade>
+                                    <Fade bottom>
+                                        <p className="aboutme-para">Born in Finland, raised in the UK. I am a graduate from the University of Kent with a passion for development.</p>
+                                    </Fade>
+                                </Col>
+                            </Row>
+                        </Container>
+                    </div>
                 </div>
                 <Skills />
                 <Projects />
