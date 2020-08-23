1a5f181db985efe7ae823cbfde61b2f37f99868d
Kristian Payne <kjp29@kent.ac.uk>
2020-08-23 22:35:22 +0100

    Edit home title

--- src/Home.js
+++ src/Home.js
@@ -12,14 +12,14 @@ class Home extends Component {
                     <div className="home" id="home">
                         <Container fluid>
                             <Row className="align-items-center">
-                                <Col sm={{ order: 12 }}>
+                                <Col lg="auto" sm={{ order: 12 }}>
                                     <Fade>
                                         <Image src={require('./img/me.jpg')} />
                                     </Fade>
                                 </Col>
                                 <Col sm={{ order: 1 }}>
                                     <Fade>
-                                        <h1 className="home-title">{"I'm a "}<span style={{ color: "#98c379" }}>{"full stack"}</span> <span style={{ color: "#61afef" }}>{"\nweb developer"}</span></h1>
+                                        <h1 className="home-title">{"I'm a "}<span style={{ color: "#98c379" }}>{"full stack"}</span> <span style={{ color: "#61afef" }}>{"developer"}</span></h1>
                                     </Fade>
                                     <Fade bottom>
                                         <p className="aboutme-para">Born in Finland, raised in the UK. I am a graduate from the University of Kent with a passion for development.</p>
